e49905dac6c95682bd4daa2d9751eb8c09d1e1c4
Dirk Schut <dschut@outlook.com>
2024-06-10 17:22:46 +0200

    Added a ball that doesn't bounce and replaced the paddle

--- Scripts/paddle.gd
+++ Scripts/paddle.gd
@@ -1,16 +1,12 @@
-extends CharacterBody2D
+extends StaticBody2D
 
 
-const SPEED = 300.0
+const SPEED = 100
 
 
 func _physics_process(delta):
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
-	move_and_slide()
+	print(delta)
+	position.x += direction * delta * SPEED;
